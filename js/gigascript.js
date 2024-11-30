0e729159b2bd2508779cefd2486a0b4c79d603ef
Juan Pablo Rueda <ruedag.jp@gmail.com>
2024-11-30 12:47:42 -0500

    Changes in sensor module and other improvements

--- js/gigascript.js
+++ js/gigascript.js
@@ -1,48 +1,31 @@
 function pintarTabla(tableBody, measureType) {
     fileName = `${measureType}-`;
-    $("#tabla").html(
-        `
-        <div class="row mb-2 text-muted">
-            <button class="btn btn-primary bi bi-cloud-download-fill" onclick="exportTableToCSV('${fileName}')">Descargar CSV</button>
-        </div>
-        <table class="table table-striped table-hover">
-            <thead>
-                <tr class="table-dark">
-                <th scope="col">#</th>
-                <th scope="col">Fecha</th>
-                <th scope="col">Valor</th>
-                <th scope="col">Tipo</th>
-                </tr>
-            </thead>
-        <tbody>
-            ${tableBody}
-        </tbody>
-        </table>
-        `
-        );
+    $("#tabla .table tbody").html(tableBody);
 }
 
-function pintarGrafico(pointsObject, title, suffix) {
+async function pintarGrafico(pointsObject, title, suffix) {
+    console.log(`suffix: ${suffix}`);
     var _widthGraph = document.getElementById("graph").clientWidth;
+    var _heightGraph = document.getElementById("graph").clientHeight;
 
-    var colors = ["#FF6347", "#00CED1", "#FFD700", "#800080", "#32CD32"]
-    var data = []
-    var idx = 0
-
+    var colors = ["#FF6347", "#00CED1", "#FFD700", "#800080", "#32CD32"];
+    var data = [];
+    var idx = 0;
 
     for (let key in pointsObject) {
         data.push({
             type: "line",
             name: key,
-            lineColor: colors[idx],
+            lineColor: colors[idx % colors.length],
             lineThickness: 3,
             dataPoints: pointsObject[key].datapoints
-        })
-        idx++
+        });
+        idx++;
     }
 
     var options = {
-        width: _widthGraph,
+        width: _widthGraph, 
+        height: _heightGraph,
         theme: "light2",
         exportEnabled: true,
         interactivityEnabled: true,
@@ -53,7 +36,6 @@ function pintarGrafico(pointsObject, title, suffix) {
             gridColor: "white",
             labelFontSize: 16,
             labelFontColor: "black",
-            //suffix: ` ${suffix}`
             title: suffix,
             titleFontSize: 16,
         },
@@ -70,6 +52,7 @@ function pintarGrafico(pointsObject, title, suffix) {
         },
         data: data
     };
+
     $("#graph").CanvasJSChart(options);
 }
 
@@ -80,7 +63,7 @@ function agregarDatosAlGrafico(newDataPoints) {
 }
 
 // BACKEND
-function cargarDatos(data, title, suffix) {
+async function cargarDatos(data, title, suffix) {
     const filteredData = Object.fromEntries(
         Object.entries(data).filter(([_, value]) => value !== null && value !== undefined)
     );
@@ -88,71 +71,49 @@ function cargarDatos(data, title, suffix) {
     const params = new URLSearchParams(filteredData).toString();
     const url = `http://localhost:8000/measurement?${params}`;
 
-    fetch(url, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        },
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            var tableBody = '';
-            var datos = data.result;
-
-            for (let i in datos) {
-                let cuenta = parseInt(i) + 1;
-
-                tableBody += '<tr>' +
-                    '<th scope="row">' + cuenta + '</th>' +
-                    '<td>' + datos[i].created_at + '</td>' +
-                    '<td>' + datos[i].value + '</td>' +
-                    '<td>' + datos[i].detail + '</td>' +
-                    '</tr>';
-            }
-            pintarTabla(tableBody);
-
-            var dataTypes = {};
-
-            datos.forEach(function (e) {
-                let key = (e.detail === "" || e.detail === null) ? 'data' : e.detail;
-                if (!dataTypes[key]) {
-                    dataTypes[key] = { datapoints: [] };
-                }
-                dataTypes[key].datapoints.push({ y: e.value, x: new Date(e.created_at) });
-            });
-
-            console.log(dataTypes);
-            pintarGrafico(dataTypes, title, suffix);
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
         });
-}
 
-function loadDashboardData() {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
 
-    backendRequest = $.ajax({
-        method: "GET",
-        url: "http://localhost:8000/getDashboardInfo",
-        contentType: "application/json",
-    });
+        const data = await response.json();
+        var tableBody = '';
+        var datos = data.result;
 
-    backendRequest.done(function (data) {
-        document.getElementById("pdSpan").innerText = data.pressureC.toFixed(2);
-        document.getElementById("piSpan").innerText = data.pressureD.toFixed(2);
-        document.getElementById("tiSpan").innerText = data.temperatureC.toFixed(2);
-        document.getElementById("tmSpan").innerText = data.tempercatureM.toFixed(2);
-        document.getElementById("vxSpan").innerText = data.vibrationX.toFixed(2);
-        document.getElementById("vySpan").innerText = data.vibrationZ.toFixed(2);
-        document.getElementById("fcSpan").innerText = data.fc.toFixed(2);
-        document.getElementById("vdcSpan").innerText = data.vdC.toFixed(2);
-    });
+        for (let i in datos) {
+            let cuenta = parseInt(i) + 1;
 
+            tableBody += '<tr>' +
+                '<th scope="row">' + cuenta + '</th>' +
+                '<td>' + datos[i].created_at + '</td>' +
+                '<td>' + datos[i].value + '</td>' +
+                '<td>' + datos[i].detail + '</td>' +
+                '</tr>';
+        }
+        pintarTabla(tableBody, title);
+
+        var dataTypes = {};
+
+        datos.forEach(function (e) {
+            let key = (e.detail === "" || e.detail === null) ? 'data' : e.detail;
+            if (!dataTypes[key]) {
+                dataTypes[key] = { datapoints: [] };
+            }
+            dataTypes[key].datapoints.push({ y: e.value, x: new Date(e.created_at) });
+        });
+
+        console.log(dataTypes);
+        pintarGrafico(dataTypes, title, suffix);
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
 }
 
 // UTILS 
@@ -168,7 +129,7 @@ const subHoursToDate = (date, n) => {
     return d;
 };
 
-function handleSearch(measureType, detailName) {
+async function handleSearch(measureType, detailName) {
     fecha1Value = document.getElementById("date1").value
     fecha1 = fecha1Value.toString().replace("T", " ");
 
@@ -186,7 +147,7 @@ function handleSearch(measureType, detailName) {
         detail: detail
     }
 
-    cargarDatos(data, measureType, getUnitsForMeasureType(measureType));
+    await cargarDatos(data, measureType, await getUnitsForMeasureType(measureType));
 }
 
 function formatearFecha(fecha) {
@@ -230,7 +191,7 @@ function getValue(name, data) {
 }
 
 // HANDLERS
-function manejarRelativeButton(interval, intervalType, measureType, detailName) {
+async function manejarRelativeButton(interval, intervalType, measureType, detailName) {
 
     endDate = new Date();
     var startDate = null;
@@ -250,88 +211,7 @@ function manejarRelativeButton(interval, intervalType, measureType, detailName) 
         detail: detail
     }
 
-    cargarDatos(data, measureType, getUnitsForMeasureType(measureType));
-}
-
-function reloadUsbData() {
-    fetch("http://localhost:8000/drives", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.json();
-        })
-        .then(data => {
-
-            let select = document.getElementById("usbselect");
-
-            // Limpiar opciones existentes
-            while (select.options.length > 0) {
-                select.remove(0);
-            }
-            // Agregar nuevas opciones
-            data.result.forEach(item => {
-                let opcion = document.createElement("option");
-                opcion.text = item.mountpoint;
-                opcion.value = item.mountpoint;
-                select.add(opcion);
-            });
-        })
-        .catch(error => {
-            console.error("Error fetching USB data:", error);
-        });
-}
-
-function exportData(measureType, detailName) {
-    console.log("Generating report :D");
-    const fecha1Value = document.getElementById("date1").value;
-
-    const fecha2Value = document.getElementById("date2").value;
-
-    const select = document.getElementById("usbselect");
-    const selected = select.value;
-
-    const detail = detailName === null ? null : document.getElementById(detailName).value;
-
-    const data = {
-        measure_type: measureType,
-        start_date: formatearFecha(new Date(fecha1Value)),
-        end_date: formatearFecha(new Date(fecha2Value)),
-        mountpoint: selected,
-        detail: detail,
-    }
-
-    const filteredData = Object.fromEntries(
-        Object.entries(data).filter(([_, value]) => value !== null && value !== undefined)
-    );
-
-    const params = new URLSearchParams(filteredData).toString();
-
-    const url = `http://localhost:8000/measurement/export?${params}`;
-
-    fetch(url, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.json();
-        })
-        .then(data => {
-            alert('Reporte generado');
-        })
-        .catch(error => {
-            console.error("Error exporting data:", error);
-        });
+    await cargarDatos(data, measureType, await getUnitsForMeasureType(measureType));
 }
 
 async function fetchBatteryLevel() {
@@ -365,43 +245,30 @@ function updateBatteryLevel(level) {
     batteryElement.textContent = level + '%';
 }
 
-function getUnitsForMeasureType(measureType){
-    return "units";
-}
+async function getUnitsForMeasureType(measureType) {
+    try {
+        const response = await fetch(`http://localhost:8000/measurement/sensor?measure_type=${measureType}`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
 
-function exportTableToCSV(filename) {
-    var csv = [];
-    var rows = document.querySelectorAll("table tr");
+        const data = await response.json();
+        const result = data.result;
 
-    for (var i = 0; i < rows.length; i++) {
-        var row = [], cols = rows[i].querySelectorAll("td, th");
-        
-        for (var j = 0; j < cols.length; j++) 
-            row.push(cols[j].innerText);
-        
-        csv.push(row.join(","));
+        if (result && result.measurement_spec) {
+            const measure = result.measurement_spec.find(m => m.measure_type === measureType);
+            if (measure) {
+                return measure.unit;
+            } else {
+                console.log(`No se encontró la unidad para el tipo de medida: ${measureType}`);
+                return null;
+            }
+        } else {
+            console.log("No se encontraron especificaciones de medida.");
+            return null;
+        }
+    } catch (error) {
+        console.error("Error al recuperar la unidad del sensor:", error);
+        return null;
     }
-
-    downloadCSV(csv.join("\n"), filename);
-}
-
-function downloadCSV(csv, filename) {
-    var csvFile;
-    var downloadLink;
-
-    csvFile = new Blob([csv], {type: "text/csv"});
-
-    downloadLink = document.createElement("a");
-
-    downloadLink.download = filename + ".csv";
-
-    downloadLink.href = window.URL.createObjectURL(csvFile);
-
-    downloadLink.style.display = "none";
-
-    document.body.appendChild(downloadLink);
-
-    downloadLink.click();
-
-    document.body.removeChild(downloadLink);
 }
